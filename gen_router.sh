48d9ef3c5a9650bb1e97d02eac55878f8193c8bf
djkloop <1044032256@qq.com>
2020-06-22 00:06:28 +0800

    update

--- gen_router.sh
+++ gen_router.sh
@@ -2,8 +2,8 @@
  # @Author       : djkloop
  # @Date         : 2020-06-21 18:25:00
  # @LastEditors  : djkloop
- # @LastEditTime : 2020-06-21 18:25:17
- # @Description  : 头部注释
+ # @LastEditTime : 2020-06-22 00:06:13
+ # @Description  : 自动生成路由
  # @FilePath     : /open_app/gen_router.sh
 ### 
 
